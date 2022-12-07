2c8f002ff8d0c57f30a21ccf43656494be18d1f1
Seonghee Yun <104363065+intacthee@users.noreply.github.com>
2022-12-07 17:44:24 +0900

    Add files via upload

---
+++ js/video02.js
@@ -0,0 +1,37 @@
+
+// var swc = true;
+
+// $('.control').on('click', function () {
+//     $(this).toggleClass('on');
+//     if (swc) {
+//         $('video').trigger('pause');
+//     } else {
+//         $('video').trigger('play');
+//     }
+
+//     //스위치 만들기 >> 참거짓을 반복하기
+//     swc = !swc; // ! :부정연산자
+// });
+
+
+// $('.control').on('click', function () {
+//     $(this).toggleClass('on');
+//     if ($(this).hasClass('on')) {
+//         $('video').trigger('pause');
+//     } else {
+//         $('video').trigger('play');
+//     }
+
+// });
+
+
+$('.control').on('click', function () {
+    $(this).toggleClass('on');
+    $(this).hasClass('on') ?
+        $('video').trigger('pause')
+        : $('video').trigger('play');
+}
+
+);
+
+
